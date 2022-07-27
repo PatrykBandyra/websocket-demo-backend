2e0ace93c6ba3cc0949476215d22a93bea26cb8e
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-07-27 12:30:31 +0200

    added allowed origins

--- src/main/java/com/example/websocketdemobackend/WebSocketConfiguration.java
+++ src/main/java/com/example/websocketdemobackend/WebSocketConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp-endpoint").withSockJS();
+        registry.addEndpoint("/stomp-endpoint").setAllowedOrigins("http://localhost:4200", "http://localhost:4200/").withSockJS();
     }
 
     @Override
